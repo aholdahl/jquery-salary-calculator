a99f6791c8b2d1b3762336e97fd7fcbe9429f8f9
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-07-28 09:13:10 -0500

    add submit keyup event, and title casing to text outputs

--- client.js
+++ client.js
@@ -4,26 +4,28 @@ let salaryTotal = 0;
 
 function readyNow() {
     $('#monthlyOut').text(salaryTotal / 12);
-    $('#addButton').on('click', addEmployee);
-    $('#employeeOut').on('click', '.deleteEmp', deleteEmployee)
+    $('#addButton').on('click', addEmployee);//At the click event within Submit button, initiate addEmployee Function
+    $('#employeeOut').on('click', '.deleteEmp', deleteEmployee)//At the click event within Delete button, initiate deleteEmployeeFunction
+    $('input').keyup(enterToClick); //At the keyup event within Input element, initiate hideError function
+
 }
 
 function addEmployee() {
     //add employee data to table on DOM
     $('#employeeOut').append(`<tr class="newEmployee">
-                    <td>${$('#inFirst').val()} </td>
-                    <td>${$('#inLast').val()} </td>
+                    <td class="titleCasing">${$('#inFirst').val()} </td>
+                    <td class="titleCasing">${$('#inLast').val()} </td>
                     <td>${$('#inEmpId').val()} </td>
-                    <td>${$('#inTitle').val()} </td>
+                    <td class="titleCasing">${$('#inTitle').val()} </td>
                     <td class="annualSalaryOut">${$('#inSalary').val()}</td>
                     <td class="deleteBtnContainer"><button class="deleteEmp btn btn-info">Delete</button></td>
                     </tr>`)
     //calculate monthly salary total
-    salaryTotal += Number($('#inSalary').val());
-    $('#monthlyOut').text((salaryTotal / 12).toFixed(2));
+    salaryTotal += Number($('#inSalary').val());//converts to number before adding
+    $('#monthlyOut').text((salaryTotal / 12).toFixed(2));//always shows two decimals
     //is monthlyOut > $20,000?
     if (salaryTotal / 12 > 20000) {
-        $('#monthlyOut').parent().addClass("alert alert-danger");
+        $('#monthlyOut').parent().addClass("alert alert-danger");//updates background color of parent element
     }
     //clear input fields
     $('#inFirst').val('');
@@ -35,13 +37,19 @@ function addEmployee() {
 
 function deleteEmployee() {
     //retrieve the annual salary sibling element of the current td and subtract from salaryTotal
-    salaryTotal -= Number($(this).parent().prev().text());
+    salaryTotal -= Number($(this).parent().prev().text()); //converts to number before adding
     //append updated salary to the DOM
-    $('#monthlyOut').text((salaryTotal / 12).toFixed(2));
+    $('#monthlyOut').text((salaryTotal / 12).toFixed(2)); //always shows two decimals
     //remove this tr from the DOM
     $(this).parent().parent().remove();
     //is monthlyOut < $20,000?
     if (salaryTotal / 12 < 20000) {
-    $('#monthlyOut').parent().removeClass("alert alert-danger");
+    $('#monthlyOut').parent().removeClass("alert alert-danger"); //updates background color of parent element
     }
 }
+
+function enterToClick(key) {
+    if (key.keyCode == 13) { //checks if enter/return key was released
+        addEmployee(); //if true, run newCar function
+    } //end if
+} //end enterToClick function
